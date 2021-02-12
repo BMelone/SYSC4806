e8f067b5915f334e9d4da45a12006ab6b9fb1cd2
Benjamin Melone <melone22@rogers.com>
2021-02-11 21:35:20 -0500

    optimized imports

--- src/test/java/com/bensoft/lab/WebMockTest.java
+++ src/test/java/com/bensoft/lab/WebMockTest.java
@@ -1,7 +1,6 @@
 package com.bensoft.lab;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
