91581376babc8ad9b3742e48af0e623f2506bc29
Benjamin Melone <melone22@rogers.com>
2021-02-11 21:55:05 -0500

    Optimized imports

--- src/main/java/com/bensoft/lab/WebController.java
+++ src/main/java/com/bensoft/lab/WebController.java
@@ -1,7 +1,5 @@
 package com.bensoft.lab;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
